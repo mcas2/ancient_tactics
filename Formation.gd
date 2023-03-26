05420e7d3b5318dba483e994c6b8f44bc2e68c85
mcas2 <mcas@tutanota.com>
2023-03-26 23:32:36 +0200

    Selectable formation

--- Formation.gd
+++ Formation.gd
@@ -1,16 +1,26 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
-export (int) var speed = 200
-
-onready var target = position
-var velocity = Vector2()
+@export var speed = 400
+var selected_texture = preload("res://images/formation_selected_blue.png")
+var default_texture = preload("res://images/formation_blue.png")
+@onready var sprite = get_node("AllyFormationSprite")
+var target = position
+var selectable = false
+var selected = false
 
 func _input(event):
-	if event.is_action_pressed("right_click"):
+	if selectable && event.is_action("right_click"):
+		selected = true
+		sprite.texture = selected_texture
+	if event.is_action("right_click") && selected:
+		look_at(get_global_mouse_position())
 		target = get_global_mouse_position()
-
-func _physics_process(__delta):
+		
+func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
-	look_at(get_global_mouse_position())
-	if position.distance_to(target) > 5:
-		velocity = move_and_slide(velocity)
+	look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
+
+func _mouse_enter():
+	selectable = true
